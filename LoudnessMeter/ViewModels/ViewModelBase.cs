0f7bf8536842dd5a3f5b09eca82ce29818444658
veron <veron@VERONI4KA-DESKT>
2023-02-10 14:34:57 +0300

    Add project files.

---
+++ LoudnessMeter/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoudnessMeter.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
