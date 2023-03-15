6d5807610edeec7f24962496d32739ef4adcd31d
Veronika <smirnyagina@speechpro.com>
2023-03-15 20:14:09 +0300

    upgrade to xplat 11

---
+++ Avalonia.LoudnessMeter/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Avalonia.LoudnessMeter.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Avalonia.LoudnessMeter/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.LoudnessMeter.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
